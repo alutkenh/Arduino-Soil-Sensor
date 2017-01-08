c21c783c0104c0dd1941f15ea432d69b4b053de0
alutkenh <alutkenh@yahoo.com>
2017-01-07 21:06:11 -0500

    Update soil_moisture_v1.ino

--- soil_moisture_v1.ino
+++ soil_moisture_v1.ino
@@ -10,54 +10,32 @@ int sensorMin = 1023;        // minimum sensor value
 int sensorMax = 0;           // maximum sensor value
 
 
+void setup() {
+  // turn on LED to signal the start of the calibration period:
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
 
-void setup(){
-  //Start Serial
-  Serial.begin(9600);
-
-  Serial.print("variables: created");
-  
-  pinMode(13, OUTPUT); //LED will stay on until calibration is complete
-  pinMode(14,OUTPUT);
-  digitalWrite(13,HIGH);
-  digitalWrite(14,LOW);
-  
-  while(millis()<5000){
+  // calibrate during the first five seconds 
+  while (millis() < 5000) {
     sensorValue = analogRead(sensorPin);
-    //sensorValue1 = analogRead(sensorPin1);
-    
-    // Record the Maximum Sensor Value
+
+    // record the maximum sensor value
     if (sensorValue > sensorMax) {
       sensorMax = sensorValue;
     }
-    
-    // Record the Minimum Sensor Value
+
+    // record the minimum sensor value
     if (sensorValue < sensorMin) {
       sensorMin = sensorValue;
     }
-    digitalWrite(13,LOW);
-    delay(500);
-    digitalWrite(13,HIGH);
   }
-  // Signal end of the calibration period
+
+  // signal the end of the calibration period
   digitalWrite(13, LOW);
-  digitalWrite(14,HIGH);
-  delay(1500);
-  digitalWrite(14,LOW);
-//  for(i = 1 : i<=5 : i++){
-//    digitalWrite(14,HIGH);
-//    delay(1500);
-//    digitalWrite(14,LOW);
-//    delay(500);
-//  }
-//  Serial.print("Max_Value\tMin_Value\n");
-//  Serial.print(sensorMax);
-//  Serial.print("\t");
-//  Serial.print(sensorMin);
 }
 
-void loop{
-   // read the sensor:
+void loop() {
+  // read the sensor:
   sensorValue = analogRead(sensorPin);
 
   // apply the calibration to the sensor reading
@@ -68,5 +46,4 @@ void loop{
 
   // fade the LED using the calibrated value:
   analogWrite(ledPin, sensorValue);
-}
 }
